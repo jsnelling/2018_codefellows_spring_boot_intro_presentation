6bd7b975fc040cc5a7a09f5dceee8da6790c6f00
jsnelling <jon.snelling@gmail.com>
2018-05-29 05:08:46 -0700

    Step 1 - Add Spring Boot to Gradle and create first controller
    This commit starts with a new Java and Gradle project and demonstrates a
    hello world controller.

---
+++ src/main/java/HelloWorldController.java
@@ -0,0 +1,45 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/*
+ *  @Controller
+ *  ********************************************************************************
+ *  Tells Spring that this class serves as a controller. It is a type of component
+ *  and in this case allows us to declare the routes which this class is responsible
+ *  for handling.
+ *
+ *  @EnableAutoConfiguration
+ *  ********************************************************************************
+ *  Tells Spring to, in addition to finding components for dependency injection, do
+ *  it's best to automatically configure spring based on the dependencies which are
+ *  included.
+ */
+@Controller
+@EnableAutoConfiguration
+public class HelloWorldController {
+
+    /*
+     *  @RequestMapping
+     *  ********************************************************************************
+     *  Attaches the index method of HelloWorldController to the root route:
+     *  http://your-site.com/ -> HelloWorldController::index
+     *
+     *  @ResponseBody
+     *  ********************************************************************************
+     *  Tells Spring that the value returned from this function should be automatically
+     *  serialized to JSON and returned to the client.
+     */
+    @RequestMapping("/")
+    @ResponseBody
+    String index() {
+        return "Hello from Spring Boot!";
+    }
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloWorldController.class, args);
+    }
+}
+
